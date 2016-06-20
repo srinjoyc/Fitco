154032b02c7a0472d7dc4242e130ad2ef4961196
Shiming Shi <Shiming@Shimings-MacBook-Air.local>
2016-06-20 14:08:07 -0700

    changed database

--- fitco/config/routes.rb
+++ fitco/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :trainers
 
+
   get '/trainers/search/:txt' => 'trainers#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
